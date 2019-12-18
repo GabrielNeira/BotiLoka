b6b907b650d84efd04dca50ca5613b454887d138
PREVIRED\jnarvaez <jnarvaez@previred.com>
2019-12-18 17:56:12 -0300

    Actualizacion

--- controller/productoController.js
+++ controller/productoController.js
@@ -2,9 +2,17 @@
 // Cargamos los modelos para usarlos posteriormente
 var ProductoModel = require('../models/productos');
 
+
+
 function getProductos(req, res){
     
    let productos = ProductoModel.find();
-   console.log(productos);
-   return res.status(200).send(productos);
+  
+   respuesta = {
+	  error: true,
+	  codigo: 200,
+	  mensaje: productos
+	 };
+	 console.log(respuesta);
+   return respuesta ;
 }
--- routes/users.js
+++ routes/users.js
@@ -2,12 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 var UserController = require('../controller/productoController');
-
+const bodyParser = require('body-parser');
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/productos', function(req, res) {
+	console.log(UserController.getProductos);
+  res.status(200).send(UserController.getProductos);
 });
 
-/* GET users listing. */
-router.route('/productos').get(UserController.getProductos);
+
 module.exports = router;
